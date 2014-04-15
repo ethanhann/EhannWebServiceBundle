05deb27c031850f7008e61145a6b1e7182e37334
Ethan Hann <eh2413@columbia.edu>
2014-04-15 00:37:14 -0400

    Add some licensing notices

--- Controller/IWebServiceController.php
+++ Controller/IWebServiceController.php
@@ -1,13 +1,11 @@
 <?php
+/**
+ * @author  Ethan Hann <ethanhann@gmail.com>
+ * @license For the full copyright and license information, please view the LICENSE
+ *          file that was distributed with this source code.
+ */
 
 namespace Ehann\Bundle\WebServiceBundle\Controller;
 
-//use Ehann\Bundle\WebServiceBundle\Request\IWebServiceRequest;
-//use Ehann\Bundle\WebServiceBundle\Request\WebServiceRequest;
-//use Ehann\Bundle\WebServiceBundle\Entity\Document;
-
 interface IWebServiceController {
-//    public function getAction(WebServiceRequest $request);
-//    public function postAction();
-//    public function deleteAction();
 }
--- Request/ParamConverter/DoctrineParamConverter.php
+++ Request/ParamConverter/DoctrineParamConverter.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author  Ethan Hann <ethanhann@gmail.com>
+ * @license For the full copyright and license information, please view the LICENSE
+ *          file that was distributed with this source code.
+ */
 
 namespace Ehann\Bundle\WebServiceBundle\Request\ParamConverter;
 
